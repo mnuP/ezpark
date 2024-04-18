b37ae57bc379e748a5af6e80ea621c586ae3215e
m.paredes <m.paredes@chimbalabs.com>
2024-04-18 17:14:02 -0500

    Started spring proyect

---
+++ src/main/java/com/dailycodework/ezpark/EzparkApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodework.ezpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzparkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dailycodework/ezpark/EzparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodework.ezpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
